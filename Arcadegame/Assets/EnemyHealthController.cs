64375c4008b26013e5a8ccb0b70e61f545719023
Hwang <suungjae9612@naver.com>
2020-07-01 16:15:40 +0900

    asset

---
+++ Arcadegame/Assets/EnemyHealthController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthController : MonoBehaviour
+{
+    public int bathp;
+    public int slimehp;
+    public int rabbithp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bathp = 25;
+        rabbithp = 50;
+        slimehp = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HPUP()
+    {
+        bathp += 25;
+        rabbithp += 50;
+        slimehp += 100;
+    }
+}
